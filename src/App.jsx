58938ad70e5762ae684830b477d0ad50b918ff03
calebcox725 <caleb.cox@flatironschool.com>
2017-04-12 14:46:45 -0400

    Messed around, have hexes, but they're bad hexes :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
-import * as React from 'react';
-import { Map, TileLayer, Polygon } from 'react-leaflet';
+import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import turf from '@turf/turf';
+import L from 'leaflet';
 import './App.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            hexes: [],
+        };
 
         this.mapClickHandler = this.mapClickHandler.bind(this);
         this.hexClickHandler = this.hexClickHandler.bind(this);
     }
 
     mapClickHandler(e) {
+        let map = this.refs.map.leafletElement;
+
+        let mapBounds = map.getBounds();
+        let bbox = [
+            mapBounds._southWest.lat,
+            mapBounds._southWest.lng,
+            mapBounds._northEast.lat,
+            mapBounds._northEast.lng,
+        ];
+        let cellSize = 6;
+        let units = 'miles';
+        let hexGrid = turf.hexGrid(bbox, cellSize, units);
+
+        hexGrid.features.forEach(feature => {
+            L.polygon(feature.geometry.coordinates[0]).addTo(map);
+        });
+
+        // This works exactly as expected, but due to Mercator projects on map, there's distortion!
+    }
+
+    hexClickHandler(e) {
         console.log(`
             Click Coordinates:
             -------------------------------
             Latitude: ${e.latlng.lat}
             Longitude: ${e.latlng.lng}
-        `);
-    }
-
-    hexClickHandler(e) {
-        console.log(`
-            The Blue Hexagon Was Clicked. Rejoice.
         `);
     }
 
@@ -37,17 +58,7 @@ class App extends React.Component {
                     url={'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'}
                     attribution={'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'}
                 />
-                <Polygon
-                    positions={[
-                        [48.86163878, -114.08415013,],
-                        [48.82402628, -114.05129934,],
-                        [48.78644166, -114.08419932,],
-                        [48.78644166, -114.14990102,],
-                        [48.82402628, -114.18280100,],
-                        [48.86163878, -114.14995021,],
-                    ]}
-                    onclick={this.hexClickHandler}
-                />
+                { this.props.hexes }
             </Map>
         );
     }
